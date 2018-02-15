532d45b25dedacdaadc1af3a51146ad18d0335d8
locphan2207 <locphan2207@gmail.com>
2018-02-15 14:13:38 -0800

    lose case

--- js/game.js
+++ js/game.js
@@ -7,6 +7,11 @@ const deltaT = 1;
 
 let data = [];
 let queue = [];
+let rScale = d3.scaleLinear() // radius scale
+.range([40, 500]); // width / number of data, divide 2 again cus this is radius scale
+
+let mScale = d3.scaleLinear() // mass scale
+.range([5, 50]);
 
 // Note: Becuase I add this script on top of the html file, so I need to make
 // an event listener to wait for all content loaded before running the code.
@@ -15,21 +20,16 @@ document.addEventListener("DOMContentLoaded", () => {
   d3.csv("./data/change.csv", (d) => {
     data = d3.shuffle(d.filter(row => row.population !== "..")); //filter the empty popoulation row, and shuffle
   });
-  // console.log(data);
 
   let svg = d3.select("svg")
     .attr("width",  800)
-    .attr("height", window.innerHeight);
+    .attr("height", window.innerHeight - 20);
 
   const gameWidth = parseInt(svg.attr("width"));
   const gameHeight = parseInt(svg.attr("height"));
 
-  let rScale = d3.scaleLinear() // radius scale
-    .range([40, 500]); // width / number of data, divide 2 again cus this is radius scale
 
-  let mScale = d3.scaleLinear() // mass scale
-    .range([5, 50]);
-
+  // Add shield:
   const shieldR = 25;
   svg.append("svg:image")
     .attr('x', gameWidth/2)
@@ -43,11 +43,25 @@ document.addEventListener("DOMContentLoaded", () => {
     .attr("transform", "translate(-25, -25)")
     .attr("xlink:href", "./images/shield.svg.png");
 
+  // Add character:
+  const characterR = 30;
+  svg.append("svg:image")
+    .attr('x', gameWidth/2)
+    .attr('y', gameHeight - characterR - 30)
+    .attr('width', characterR * 2)
+    .attr('height', characterR * 2)
+    .attr('r', characterR - 10) // for calculating
+    .attr('cx', gameWidth/2)
+    .attr('cy', gameHeight - characterR - 30)
+    .attr("id", "character")
+    .attr("transform", "translate(-30, -30)")
+    .attr("xlink:href", "./images/icons8-minion-50.png");
+
   // Mouse event:
   const shield = document.getElementById('shield');
   document.addEventListener("mousemove", (e) => {
-    shield.setAttribute("x", e.pageX - (window.innerWidth * 0.25)); // shift because shield position depends on svg position
-    shield.setAttribute("cx", e.pageX - (window.innerWidth * 0.25));
+    shield.setAttribute("x", e.pageX - 500); // shift because shield position depends on svg position
+    shield.setAttribute("cx", e.pageX - 500);
   });
   // Mouse click:
   let isPaused = false;
@@ -73,14 +87,13 @@ document.addEventListener("DOMContentLoaded", () => {
     rScale.domain([2851, 275067797]);
     mScale.domain([2851, 275067797]);
 
-    // Loading data to queue
-    // The condition is to make sure queue only loads meaningful data.
-    // It also helps dodging when d3.csv has not finished loading data
+    // Loading data to queue:
+    // idx = createUpdateCircles(idx, gameWidth, gameHeight);
     if (queue.length < 5) { // increase queue length by changing this number
-      if (!data[idx++]) idx++;
+      if (!data[idx]) idx++;
       else queue.push(data[idx++]);
     } else {
-      const group = svg.selectAll(".group");
+      const group = d3.select("svg").selectAll(".group");
       const groupEnter = group
         .data(queue)
         .enter().append("g").attr("class", "group");
@@ -124,32 +137,102 @@ document.addEventListener("DOMContentLoaded", () => {
 
       group.data(queue).exit().remove();
     }
+    const character = document.getElementById('character');
 
-    const circles = document.getElementsByClassName("data-circle");
-    for (let i = 0; i < circles.length; i++) {
-      // Physics:
-      let Fnet;
-      let hasCollision = false;
-      if (MyMath.detectCollision(circles[i], shield)) {
-        Fnet = MyMath.getFnet(
-          MyMath.getFg(circles[i]),
-          MyMath.getFs(circles[i], shield)
-        );
-        hasCollision = true;
-      } else Fnet = MyMath.getFnet(MyMath.getFg(circles[i]));
-      MyMath.updatePos(circles[i], Fnet, deltaT);
-      if (isOutOfFrame(circles[i])) {
-        d3.select(circles[i].parentNode).remove(); //remove group (circle + text)
-        queue.splice(idxToRemove(idx-1), 1);
-        // queue = queue.slice(1);
-      }
-    }
+    shieldCollision(idx);
     frameId = window.requestAnimationFrame(runFrame); // a better version of setInterval, call functino again inside function
+    characterCollision(frameId, idx); // have to call this after requestFrame..
   }
   frameId = window.requestAnimationFrame(runFrame); // start frame, use cancelAnimationFrame(frameId);
-
-
 });
+
+function characterCollision(frameId, idx) {
+  const circles = document.getElementsByClassName("data-circle");
+  const character = document.getElementById("character");
+  for (let i = 0; i < circles.length; i++) {
+    if (MyMath.detectCollision(circles[i], character)) {
+      console.log('YOU LOSE');
+      window.cancelAnimationFrame(frameId);
+      // debugger;
+    }
+  }
+}
+
+function shieldCollision(idx) {
+  const circles = document.getElementsByClassName("data-circle");
+  const shield = document.getElementById("shield");
+  for (let i = 0; i < circles.length; i++) {
+    // Physics:
+    let Fnet;
+    let hasCollision = false;
+    if (MyMath.detectCollision(circles[i], shield)) {
+      Fnet = MyMath.getFnet(
+        MyMath.getFg(circles[i]),
+        MyMath.getFs(circles[i], shield)
+      );
+      hasCollision = true;
+    } else Fnet = MyMath.getFnet(MyMath.getFg(circles[i]));
+    MyMath.updatePos(circles[i], Fnet, deltaT);
+    if (isOutOfFrame(circles[i])) {
+      d3.select(circles[i].parentNode).remove(); //remove group (circle + text)
+      queue.splice(idxToRemove(idx-1), 1);
+    }
+  }
+}
+
+// function createUpdateCircles(idx, gameWidth, gameHeight) {
+//   // The condition is to make sure queue only loads meaningful data.
+//   // It also helps dodging when d3.csv has not finished loading data
+//   if (queue.length < 5) { // increase queue length by changing this number
+//     if (!data[idx]) return idx+1;
+//     else queue.push(data[idx]);
+//   } else {
+//     const group = d3.select("svg").selectAll(".group");
+//     const groupEnter = group
+//       .data(queue)
+//       .enter().append("g").attr("class", "group");
+//     groupEnter
+//       .append("circle")
+//         .attr("cx", (d) => gameWidth/6 + Math.random()*gameWidth/3 ) //random position from 1/3 screen to 2/3 screen
+//         .attr("cy", (d,i) => i*-400)
+//         .attr("r", d => rScale(d.population))
+//         .attr("fill", (d) => {
+//           if (d.name[0] === "F") {
+//             return "lightcoral";
+//           } else {
+//             return "lightblue";
+//           }
+//         })
+//         .attr("fill-opacity", 0.7)
+//         .attr("class", "data-circle")
+//         .attr("m", (d) => mScale(d.population) )
+//         .attr("vx", 0) // initialize initial velocity:
+//         .attr("vy", 0);
+//     groupEnter
+//       .append("text")
+//         .attr("fill", (d) => {
+//           if (d.name[0] === "F") {
+//             return "navy";
+//           } else {
+//             return "darkred";
+//           }
+//         })
+//         .attr("font-family", "sans-serif")
+//         .attr("font-size", "15px")
+//         .attr("text-anchor", "middle")
+//         .text((d) => d.country);
+//     groupEnter
+//       .append("text")
+//         .attr("fill", "black")
+//         .attr("text-anchor", "middle")
+//         .attr("font-family", "sans-serif")
+//         .attr("dy", 20) //shift 10px down
+//         .text((d) => d.population);
+//
+//     group.data(queue).exit().remove();
+//     return idx+1; // to update idx in game loop
+//   }
+// }
 
 function isOutOfFrame(htmlCircle) {
   const cx = parseInt(htmlCircle.getAttribute("cx"));
